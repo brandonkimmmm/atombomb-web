62d3ee8e0e7211a2fe98385f290ce8f0df81fe47
Brandon Kim <bkim2490@gmail.com>
2020-10-26 22:48:02 +0900

    basic template added

---
+++ src/containers/landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+	Container
+} from '@material-ui/core';
+
+export const Landing = () => {
+	return (
+		<Container>
+			<h1>Welcome</h1>
+		</Container>
+	)
+};
