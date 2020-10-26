e5b8210c736668226785b56225e9e937e4e155f6
Brandon Kim <bkim2490@gmail.com>
2020-10-26 23:34:30 +0900

    axios and user slice created

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,23 +1,39 @@
-// import {
-// 	createSlice,
-// 	createAsyncThunk,
-// 	createEntitiyAdapter
-// } from '@reduxjs/toolkit';
+import {
+	createSlice,
+	createAsyncThunk
+} from '@reduxjs/toolkit';
 
-// export const logIn = createAsyncThunk
+import client from '../../api';
 
-// const initialState = {
-// 	loggedIn: false,
-// 	token: '',
-// 	info: {}
-// }
+export const loginUser = createAsyncThunk('user/loginUser', async (email, password) => {
+	const response = await client.post('/login', {
+		email,
+		password
+	});
+	return {
+		token: response.token,
+		email
+	}
+});
 
-// const userSlice = createSlice({
-// 	name: 'user',
-// 	initialState,
-// 	reducers: {
-// 		logIn(state, action) => {
+const initialState = {
+	loggedIn: false,
+	token: '',
+	email: ''
+}
 
-// 		}
-// 	}
-// })
+const userSlice = createSlice({
+	name: 'user',
+	initialState,
+	reducers: {},
+	extraReducers: {
+		[loginUser.fulfilled]: (state, action) => {
+			const { email, token } = action.payload;
+			state.token = token;
+			state.email = email;
+			state.loggedIn = true;
+		}
+	}
+})
+
+export default userSlice.reducer;
