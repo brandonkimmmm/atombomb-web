f9a23da2f4d4cfcdf5e555012c59ec25538447e6
Brandon Kim <bkim2490@gmail.com>
2020-11-06 19:42:00 +0900

    landing functionality cards

--- src/components/LandingFunctionalities/styles.js
+++ src/components/LandingFunctionalities/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(theme => ({
 	heroContent: {
-		backgroundColor: theme.palette.background.paper,
+		backgroundColor: theme.palette.text.primary,
 		padding: theme.spacing(8, 0, 6),
 	},
 	heroButtons: {
