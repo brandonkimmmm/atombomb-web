4d526b989bd4d808a24303f8819f9cd84f7ede95
Brandon Kim <bkim2490@gmail.com>
2020-11-09 23:06:27 +0900

    dasboard redirect

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,13 +1,25 @@
 import React, { Fragment } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { useSelector } from 'react-redux';
+import { getUser } from '../../redux/user/userSlice';
+import { Redirect } from 'react-router';
 
 export const Dashboard = () => {
-	return (
-		<Fragment>
-			<CssBaseline />
-				<main>
-					Herro
-				</main>
-		</Fragment>
-	);
+	const user = useSelector(getUser);
+
+	const renderedPage = () => {
+		if (!user.loggedIn) {
+			return <Redirect to="/" />
+		} else {
+			return (
+				<Fragment>
+					<CssBaseline />
+						<main>
+							Herro
+						</main>
+				</Fragment>
+			)
+		}
+	}
+	return renderedPage();
 }
