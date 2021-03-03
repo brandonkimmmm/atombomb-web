93e9893f011d9031b9ea047f087a6301f2d676df
Brandon Kim <bkim2490@gmail.com>
2021-03-03 22:12:19 +0900

    supported social media adde

--- src/containers/Landing/index.js
+++ src/containers/Landing/index.js
@@ -14,7 +14,7 @@ export const Landing = () => {
 			return (
 				<Fragment>
 					<main className='flex flex-col h-screen'>
-						<div className='flex flex-row bg-black justify-between p-12 space-x-20 h-2/3'>
+						<div className='flex flex-row bg-black justify-between items-center p-12 space-x-20 h-2/3'>
 							<div className='flex flex-col justify-center w-1/2'>
 								<div className='text-7xl mb-6 font-extrabold text-white'>
 									Use peer pressure to your advantage
@@ -29,9 +29,9 @@ export const Landing = () => {
 									<input type='submit' value='Sign up to Atom Bomb!' className='font-bold text-2xl bg-green-400 text-white p-4 rounded-md' />
 								</form>
 							</div>
-							<img className='w-1/2' src='https://www.pinclipart.com/picdir/big/526-5260657_bomb-outline-black-and-white-clip-art-at.png' />
+							<img className='w-1/2 object-contain h-1/2' src='https://www.pinclipart.com/picdir/big/526-5260657_bomb-outline-black-and-white-clip-art-at.png' />
 						</div>
-						<div className='flex flex-row space-x-10 p-10'>
+						<div className='flex flex-row space-x-10 p-10 h-full'>
 							<div className='flex flex-col border-4 border-black rounded bg-yellow-200 px-4 py-10'>
 								<img className='rounded mb-8' src='https://cdn.cms-twdigitalassets.com/content/dam/business-twitter/solutions/twitter-ads-social-share.jpg.twimg.768.jpg' />
 								<div className='text-xl font-bold '>Connect</div>
@@ -48,9 +48,13 @@ export const Landing = () => {
 								<div className='text-lg font-light'>Complete your tasks or have your bomb message posted on twitter</div>
 							</div>
 						</div>
-						{/* <div className='flex flex-row'>
-							Hello
-						</div> */}
+						<div className='flex flex-col bg-black p-10 items-center space-y-10'>
+							<div className='text-white text-5xl font-extrabold'>Supported Social Media</div>
+							<div className='flex-row'>
+								<img className='object-scale-down h-20' src='https://cdn4.iconfinder.com/data/icons/social-media-icons-the-circle-set/48/twitter_circle-512.png' />
+							</div>
+							<div className='text-white font-light text-lg'>More to come!</div>
+						</div>
 					</main>
 				</Fragment>
 			)
